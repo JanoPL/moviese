b21c3574822b09698dfb2735d88c26ed36ccafac
JanoPL <xjanusz4@gmail.com>
2023-11-27 13:14:29 +0100

    feat: update migration

---
+++ migrations/Version20231127121407.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231127121407 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE entertainment_product (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(128) NOT NULL, type VARCHAR(25) DEFAULT NULL, discr VARCHAR(255) NOT NULL, genre VARCHAR(128) DEFAULT NULL, season_number INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE entertainment_product');
+    }
+}
