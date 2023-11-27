6d3fff6cfaf1217009d7e40d874ca8d34ac07fe6
JanoPL <xjanusz4@gmail.com>
2023-11-27 13:10:40 +0100

    feat: enable preprocessor css

--- assets/app.js
+++ assets/app.js
@@ -6,34 +6,9 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/global.scss';
 
 const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
-
-let hideShowFields = function(selectedValue) {
-    // Copy elements into an array with the "spread operator" ... so we can safely use ".forEach"
-    let allElements = [...document.querySelectorAll('[data-hide-show-me]')];
-    if (parseInt(selectedValue) === 1) {
-        allElements.forEach(item => item.classList.remove('hidden-class'));
-    } else {
-        allElements.forEach(item => item.classList.add('hidden-class'));
-    }
-};
-
-// wait for page load
-window.onload = function() {
-    let selectElement = document.querySelector('[data-select-hide-show]');
-
-    // Only if element exists
-    if(!!selectElement) {
-        selectElement.addEventListener('change', function () {
-            hideShowFields(this.value);
-        });
-
-        // also trigger once on pageload
-        hideShowFields(selectElement.value);
-    }
-};
